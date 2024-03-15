61becb8caf02e8b517506cbcb290e3894393e4b2
Julia Peter <juliagpeter0@gmail.com>
2024-03-15 01:49:13 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,47 @@
+
+
+
+function sortear() {
+    let quantidade = parseInt(document.getElementById('quantidade').value); // se não tivesse o .value ele iria recuperar o input HTML e não o valor digitado
+    let de = parseInt(document.getElementById('de').value); // parseInt FORÇA o retorno de um INTEIRO
+    let ate = parseInt(document.getElementById('ate').value);
+    
+    let sorteados = [];
+    let numeroSorteado;
+
+    for (let i = 0; i < quantidade; i++) {
+        numeroSorteado = numeroAleatorio(de, ate);
+        while (sorteados.includes(numeroSorteado)) {
+            numeroSorteado = numeroAleatorio(de, ate);
+        }
+        sorteados.push(numeroSorteado);
+    }
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteados}</label>`
+    alterarStatusBotao();
+}
+
+function reiniciar() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
+}
+
+function numeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max-min + 1)) + min;
+}
+
+function alterarStatusBotao() {
+    let botao = document.getElementById('btn-reiniciar');
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+}
+
